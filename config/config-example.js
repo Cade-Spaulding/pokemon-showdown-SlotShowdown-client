849c13f48b59fe4cfd32c2fa66cb06bc759bdeeb
Cade Spaulding <51171344+Cade-Spaulding@users.noreply.github.com>
2026-09-25 14:07:36 -0500

    Modify default server settings in config-example.js
    Updated default server configuration with new values.

--- config/config-example.js
+++ config/config-example.js
@@ -18,14 +18,7 @@ Config.whitelist = [
 
 // `defaultserver` specifies the server to use when the domain name in the
 // address bar is `Config.routes.client`.
-Config.defaultserver = {
-	id: 'showdown',
-	host: 'modified-showdown.up.railway.app',
-	port: 443,
-	httpport: 8000,
-	altport: 80,
-	registered: true
-};
+Config.defaultserver = { id: 'modifiedshowdown', host: 'modified-showdown.up.railway.app', port: 443, httpport: 80, altport: 80, registered: false };
 
 Config.roomsFirstOpenScript = function () {
 };
